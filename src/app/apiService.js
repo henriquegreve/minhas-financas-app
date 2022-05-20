fff4eccea792a271a561c81568e1484369b20208
henriquegreve <xb9gx5zs1>
2022-05-20 16:21:37 -0300

    Adicionando URL da API remota

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-    baseURL: 'http://192.168.15.74:8080'
+    baseURL: 'https://greve-minhas-financas-api.herokuapp.com'
 })
 
 class ApiService {
